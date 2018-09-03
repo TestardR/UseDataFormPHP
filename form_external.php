5f80e3c9e0ecceb7b5c74f2f70bbbda8f21c1afb
Romain Testard <40339476+TestardR@users.noreply.github.com>
2018-09-03 11:39:57 +0200

    Add files via upload

---
+++ form_external.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Checking for Form Submission</title>
+</head>
+<body>
+
+<form action="form_process.php" method="post">
+
+
+<input type="name"  name = "username" placeholder="Enter Username"></input>
+<br>
+<input type="password" name="password" placeholder="Password"></input>
+<br>
+<input type="submit" name="submit"></input>
+</form>
+</body>
+</html>
+
+
+
+
